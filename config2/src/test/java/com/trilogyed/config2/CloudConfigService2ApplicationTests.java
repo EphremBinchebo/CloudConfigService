c4e998f2adcc63b4d934ba8fd3c54da0ebe1cc1c
Ephrem Binchebo <ephremgensa1@gmail.com>
2022-07-20 14:53:47 -0400

    config2 added

---
+++ config2/src/test/java/com/trilogyed/config2/CloudConfigService2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trilogyed.config2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudConfigService2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
